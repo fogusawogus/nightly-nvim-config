f77f385bbdd5db5d23d8dd539720815c073c1c93
Jacob Wong <jacob.ka.wong@gmail.com>
2025-10-04 17:16:48 -0500

    Edits

--- lua/plugin/blink.lua
+++ lua/plugin/blink.lua
@@ -1,41 +1,46 @@
 vim.pack.add({
-    { src = 'https://github.com/Saghen/blink.cmp', version = vim.version.range('1.*') }
+	{ src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("1.*") },
 })
 
-require('blink.cmp').setup({
-      cmdline = { enabled = false },
-      fuzzy = {implementation = 'rust'},
+require("blink.cmp").setup({
+	cmdline = { enabled = false },
+	fuzzy = { implementation = "rust" },
 
-      keymap = {
-        preset = 'default',
-        ['<C-l>'] = { 'snippet_forward', 'fallback' },
-        ['<C-h>'] = { 'snippet_backward', 'fallback' },
-      },
+	keymap = {
+		preset = "default",
+		["<C-l>"] = { "snippet_forward", "fallback" },
+		["<C-h>"] = { "snippet_backward", "fallback" },
+	},
 
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono',
-      },
-
-      completion = {
-        -- By default, you may press `<c-space>` to show the documentation.
-        -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500, window = { border = 'rounded' } },
-        menu = {
-          auto_show = true,
-          border = 'none',
+	appearance = {
+		-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+		-- Adjusts spacing to ensure icons are aligned
+		nerd_font_variant = "mono",
+	},
+    signature = {
+        enabled = true,
+        window = {
+            show_documentation = false
         },
-        ghost_text = {
-          enabled = false,
-        },
+    },
 
-        list = {
-          selection = {
-            preselect = true,
-            auto_insert = false,
-          },
-        },
-      },
+	completion = {
+		-- By default, you may press `<c-space>` to show the documentation.
+		-- Optionally, set `auto_show = true` to show the documentation after a delay.
+		documentation = { auto_show = false, auto_show_delay_ms = 500, window = { border = "rounded" } },
+		menu = {
+			auto_show = true,
+			border = "none",
+		},
+		ghost_text = {
+			enabled = false,
+		},
 
+		list = {
+			selection = {
+				preselect = true,
+				auto_insert = false,
+			},
+		},
+	},
 })
