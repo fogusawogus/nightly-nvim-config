894e2eb44d69ec7a756ae20464a66feb326e488c
Jacob Wong <jacob.ka.wong@gmail.com>
2025-10-12 11:18:19 -0500

    Add eldoc

--- lua/plugin/eldoc.lua
+++ lua/plugin/eldoc.lua
@@ -3,3 +3,8 @@ vim.pack.add({
 })
 
 require('nvim-eldoc').setup()
+vim.cmd([[
+set updatetime=700           " Show signature after 700ms cursor idle
+hi link Eldoc    Normal      " Highlight for eldoc
+hi link EldocCur Identifier  " Highlight for currently focused parameter
+]])
